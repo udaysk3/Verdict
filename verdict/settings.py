e2bbed6ac1939cda6c1306a91c17117bcfd8a695
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-11-24 21:15:44 +0530

    Deploy changes

--- verdict/settings.py
+++ verdict/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://verdict-production.up.railway.app']
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
@@ -129,13 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS  = [
-    os.path.join(BASE_DIR, 'static')
-]
-# Base url to serve media files
-MEDIA_URL = '/images/'
-# Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
